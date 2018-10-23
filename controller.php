6cd2060dbe752ef2c3d4af5fe271c4ee1b8c8015
Darstnfrd <darren.sa90@gmail.com>
2018-10-23 15:16:03 +1300

    Hello world!

---
+++ controller.php
@@ -0,0 +1,75 @@
+<?php
+
+//include_once("model/Model.php");
+
+class Controller {  
+     public $model;
+     public $title;
+     public $path;   
+  
+     public function __construct()    
+     {    
+        //$this->model = new Model();
+        $this->path = '/' . @$_GET['url'];  
+     }   
+     
+     public function invoke()  
+     {
+
+     	function getCurrentUri()
+    	{
+	        $basepath = implode('/', array_slice(explode('/', $_SERVER['SCRIPT_NAME']), 0, -1)) . '/';
+	        $uri = substr($_SERVER['REQUEST_URI'], strlen($basepath));
+	        if (strstr($uri, '?')) $uri = substr($uri, 0, strpos($uri, '?'));
+	        $uri = '/' . trim($uri, '/');
+	        return $uri;
+    	}
+
+     	$base_url = getCurrentUri();
+		
+     	if ($base_url === '/') {
+    		// Home page
+    		$title = "Template Page";
+			include("view/header.php");
+
+			include("view/home.php");
+
+		}else if (preg_match('/^\/services$/i', $base_url)) {
+    		// Hand off to another router to handle /grouping/* URIs
+			$title = "Services | Template Page";
+			include("view/header.php");
+
+			include("view/services.php");
+				
+
+		}else if (preg_match('/^\/categories$/i', $base_url)) {
+			$title = "Categories | Template Page";
+			include("view/header.php");
+
+			include("view/categories.php");
+
+		}else if (preg_match('/^\/contact$/i', $base_url)) {
+			$title = "Contact | Template Page";
+			include("view/header.php");
+
+			include("view/contact.php");
+
+		}else if (preg_match('/^\/about$/i', $base_url)) {
+			$title = "About | Template Page";
+			include("view/header.php");
+
+			include("view/about.php");
+
+		}else{
+			// Handle that 404
+			$title = "404 Error | Template Page";
+			include("view/header.php");
+
+			include("view/404.php");
+		}		
+
+        include("view/footer.php");
+	} 
+}
+
+?>
---
+++ view/services.php
@@ -0,0 +1,7 @@
+<div class="container">
+	<h1>Services</h1>
+	<p>This is a paragraph!</p>
+	
+
+
+</div>
